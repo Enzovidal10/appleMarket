6b39edbab3bf3eba3b609dee6912c764684d9d9c
Enzovidal10 <enzovidal2001@gmail.com>
2022-07-19 15:07:05 -0300

    Entrega N°2 JS

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ alert("Bienvenido, para consultar si su producto cuenta con el servicio de enví
 let numero= parseInt(prompt("Ingrese el costo de su producto para consutar el envío gratis")); 
 
 if (numero<400) {
-    alert("Su producto tiene un costo de " + numero + " dólares, el costo del envío es de un 10% sumado al costo del producto")  
+    alert("Su producto tiene un costo de " + numero + " dólares, el costo del envío es de un 10% sumado al costo del producto. "+"COSTO TOTAL + ENVÍO: "+(numero+numero*0.10)+" dólares.")  
 } else {
     alert("Su producto tiene un costo de " + numero + " dólares, por lo cual tiene el envío gratis")  
 }
@@ -13,14 +13,14 @@ if (numero<400) {
 
 //Lista de compras de productos
 let producto="";
-alert("Bienvenidos a Apple Market, esta es una lista de productos a comprar, para finalizar la lista escriba ESC")
+alert("Bienvenidos a Apple Market, esta es una lista de productos a comprar, para finalizar la lista escriba SALIR.")
 
 
-while (producto!="ESC"){
+while (producto!="SALIR"){
     producto=prompt("Ingrese el nombre de un producto");
     producto=producto.toUpperCase();
     console.log(producto)
-    alert("Usted agregó "+producto);
+    alert("Usted agregó "+producto)
 }
 alert("Gracias por comprar nuestros productos")
 
@@ -56,15 +56,25 @@ switch (modelo) {
 
 //Detalles de los productos
 
-// function productoMovil(nombre, modelo,almacenamiento,precio){
-//     this.name=nombre
-//     this.model=modelo
-//     this.storage=almacenamiento
-//     this.price=precio
-// }
+function productoMovil(nombre, modelo,almacenamiento,precio){
+    this.name=nombre
+    this.model=modelo
+    this.storage=almacenamiento
+    this.price=precio
+}
 
-// const producto1= new productoMovil("Iphone","XR","128GB","USD 1050");
-// console.log(producto1)
+const producto1= new productoMovil("Iphone","XR","128GB","USD 1050");
+const producto2= new productoMovil("Iphone","XR","64GB","USD 950");
+const producto3= new productoMovil("Iphone","XR","32GB","USD 750");
+const producto4= new productoMovil("Iphone","X","128GB","USD 900");
+const producto5= new productoMovil("Iphone","X","64GB","USD 650");
+console.log(producto1)
+console.log(producto2)
+console.log(producto3)
+console.log(producto4)
+console.log(producto5)
+
+//Ciclo con for
 
 // for(propiedad in producto1){
 //     console.log(propiedad+ ": "+producto1[propiedad])

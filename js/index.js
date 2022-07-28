18534c924cc52b5c65dd9141579c8eccf17a7494
Enzovidal10 <enzovidal2001@gmail.com>
2022-07-28 15:52:03 -0300

    Primer entrega del pryecto final

--- js/index.js
+++ js/index.js
@@ -1,138 +1,121 @@
 
-// //Consultar envío gratis
-// alert("Bienvenido, para consultar si su producto cuenta con el servicio de envíos gratis debe ingresar su costo total. Este servicio solamente esta disponible para productos con un costo mayor a USD 400.");
-
-// let numero= parseInt(prompt("Ingrese el costo de su producto para consutar el envío gratis")); 
-
-// if (numero<400) {
-//     alert("Su producto tiene un costo de " + numero + " dólares, el costo del envío es de un 10% sumado al costo del producto. "+"COSTO TOTAL + ENVÍO: "+(numero+numero*0.10)+" dólares.")  
-// } else {
-//     alert("Su producto tiene un costo de " + numero + " dólares, por lo cual tiene el envío gratis")  
-// }
  
+//Buscar Modelos
+alert("Ingresa el modelo de IPhone o Mac, que desea comprar")
 
-// //Lista de compras de productos
-// let producto="";
-// alert("Bienvenidos a Apple Market, esta es una lista de productos a comprar, para finalizar la lista escriba SALIR.")
+let modelo=prompt("Escribe el modelo en el que estas interesado/a");
+modelo=modelo.toUpperCase();
 
+switch (modelo) {
+    case "IPHONE XR":
+        alert("Has elegido el " + modelo+" Tenemos en stock")
+        break;
+    case "IPHONE X":
+        alert("Has elegido el " + modelo+" Tenemos en stock")
+        break;
+    case "MACBOOK AIR":
+        alert("Has elegido el " + modelo+" Tenemos en stock")
+    break;
+    case "MACBOOK 15":
+        alert("Has elegido el " + modelo+" Tenemos en stock")
+    break;
+    case "IPHONE 8":
+        alert("Has elegido el " + modelo+" Tenemos en stock")
+    break;
+    case "IPHONE 7":
+        alert("Has elegido el " + modelo+" Tenemos en stock")
+    break;
+    default:
+        alert("No disponemos en stock actualmente")
+}
 
-// while (producto!="SALIR"){
-//     producto=prompt("Ingrese el nombre de un producto");
-//     producto=producto.toUpperCase();
-//     console.log(producto)
-//     alert("Usted agregó "+producto)
-// }
-// alert("Gracias por comprar nuestros productos")
+//Función Detalles de los productos 
+function productoMovil(modelo,color,almacenamiento,precio){
+    this.model=modelo
+    this.color=color
+    this.storage=almacenamiento
+    this.price=precio
+}
 
+const producto1= new productoMovil("Iphone XR","Blanco y Negro","128GB","USD 1050");
+const producto2= new productoMovil("Iphone XR","Dorado","64GB","USD 950");
+const producto3= new productoMovil("Iphone XR","Negro y Dorado","32GB","USD 750");
+const producto4= new productoMovil("Iphone X","Blanco","128GB","USD 900");
 
-// //Buscar Modelos
-// alert("Ingresa el modelo de celular o mac siguiendo con un -, ejemplo IPHONE-XR, MACBOOK-AIR")
+console.log("Aquí se muestran los productos que ya están agregados de la Función")
+console.log(producto1)
+console.log(producto2)
+console.log(producto3)
+console.log(producto4)
 
-// let modelo=prompt("Escribe el modelo en el que estas interesado/a");
-// modelo=modelo.toUpperCase();
+//Ingresar manualmente un producto
+const producto5= new productoMovil(prompt("Ingrese modelo"),prompt("Ingrese color"),prompt("Ingrese almacenamiento"),prompt("Ingrese precio"));
 
-// switch (modelo) {
-//     case "IPHONE-XR":
-//         alert("Has elegido el " + modelo)
-//         break;
-//     case "IPHONE-X":
-//         alert("Has elegido el " + modelo)
-//         break;
-//     case "MACBOOK-AIR":
-//     alert("Has elegido el " + modelo)
-//     break;
-//     case "MACBOOK-15":
-//     alert("Has elegido el " + modelo)
-//     break;
-//     case "IPHONE-8":
-//     alert("Has elegido el " + modelo)
-//     break;
-//     case "IPHONE-7":
-//     alert("Has elegido el " + modelo)
-//     break;
-//     default:
-//         alert("No disponemos en stock actualmente")
-// }
-
-// //Detalles de los productos
-
-// function productoMovil(nombre, modelo,almacenamiento,precio){
-//     this.name=nombre
-//     this.model=modelo
-//     this.storage=almacenamiento
-//     this.price=precio
-// }
-
-// const producto1= new productoMovil("Iphone","XR","128GB","USD 1050");
-// const producto2= new productoMovil("Iphone","XR","64GB","USD 950");
-// const producto3= new productoMovil("Iphone","XR","32GB","USD 750");
-// const producto4= new productoMovil("Iphone","X","128GB","USD 900");
-// const producto5= new productoMovil("Iphone","X","64GB","USD 650");
-// console.log(producto1)
-// console.log(producto2)
-// console.log(producto3)
-// console.log(producto4)
-// console.log(producto5)
+console.log("Aquí se muestran el producto agregado recientemente")
+console.log(producto5)
 
 //ARRAY con Objetos
 let productos=[
     //Iphone XR
     {"Modelo":"IPHONE XR",
-     "Almacenamiento":"64GB",
      "Color":"Azul,Dorado,Negro",
+     "Almacenamiento":"64GB",
      "Camara":"48 MP",
      "Precio":1000},
 
      {"Modelo":"IPHONE XR",
-     "Almacenamiento":"128GB",
      "Color":"Azul,Blanco,Negro",
+     "Almacenamiento":"128GB",
      "Camara":"48 MP",
      "Precio":1300},
      
      //Iphone 13
      {"Modelo":"IPHONE 13",
-     "Almacenamiento":"64GB",
      "Color":"Dorado,Negro",
+     "Almacenamiento":"64GB",
      "Camara":"64 MP",
      "Precio":1400},
 
      {"Modelo":"IPHONE 13",
-     "Almacenamiento":"128GB",
      "Color":"Blanco,Negro",
+     "Almacenamiento":"128GB",
      "Camara":"64 MP",
      "Precio":1600},
 ];
 
-console.log(productos)
-
-//For para mostrar cada uno de los productos ingresados hasta el momento
-for (let i = 0; i < productos.length; i++) {
-   console.log(productos[i])
-    
-}
-
 //Nuevos modelos de Iphone X en ultimo lugar.
 productos.push({"Modelo":"IPHONE X",
-"Almacenamiento":"32GB",
 "Color":"Azul,Dorado,Negro",
+"Almacenamiento":"32GB",
 "Camara":"48 MP",
 "Precio":700})
 
 productos.push({"Modelo":"IPHONE X",
-"Almacenamiento":"64GB",
 "Color":"Azul,Dorado,Negro,Blanco",
-"Camara":"48 MP",
-"Precio":700})
-console.log(productos)
-
-//Agregué nuevo modelo iphone XR en primer lugar.
-productos.unshift({"Modelo":"IPHONE XR",
-"Almacenamiento":"32GB",
-"Color":"Azul,Dorado,Negro",
+"Almacenamiento":"64GB",
 "Camara":"48 MP",
 "Precio":900})
 
-console.log(productos)
+//Agregué nuevo modelo iphone XR en primer lugar.
+productos.unshift({"Modelo":"IPHONE XR",
+"Color":"Azul,Dorado,Negro",
+"Almacenamiento":"32GB",
+"Camara":"48 MP",
+"Precio":900})
 
-//Mostrar ultimo IphoneX
-console.log(productos[6])
+console.log("Aquí se muestran los productos del ARRAY")
+//For para mostrar cada uno de los productos ingresados hasta el momento
+for (let i = 0; i < productos.length; i++) {
+    console.log(productos[i])   
+ }
+
+ //filter para buscador
+ let ingreso=prompt("Ingresar el modelo en el buscador");
+ ingreso=ingreso.toUpperCase()
+ alert("Usted ingresó "+ingreso)
+
+const  filtro= productos.filter(producto => producto.Modelo===ingreso);
+console.log(filtro)
+
+
+
--- js/indexparapracticar.js
+++ js/indexparapracticar.js
@@ -29,3 +29,40 @@
 //Eliminar de a varios datos a la vez
 //clientes.splice(1, 2)
 
+
+// //Mostrar ultimo Iphone ingresado
+// console.log(productos[6])
+
+//DOM
+
+// getElementById('id')
+// getElementByClassName('className')
+// getElementByTagName('tagName')
+
+// const titles = document.getElementsByClassName('title')
+// const bienvenida = document.getElementById('bienvenida')
+// const likes = document.getElementsByClassName('like')
+
+//console.log(title)
+//console.log(bienvenida.innerHTML)
+//console.log(like)
+
+//innerHTML y innerText
+//bienvenida.innerHTML = 'Hola, <span style="color:red">cambie mi primer texto</span>'
+
+//console.log(titles)
+//console.log(typeof(bienvenida.innerHTML))
+//Bienvenidxa a mi pagina
+
+//console.log(like)
+// const colors = ['red', 'yellow', 'purple', 'green', 'blue']
+
+// //pasar numeros del 0 al 4
+// //bienvenida.style.color = colors[]
+// //console.log(Math.floor(Math.random()*colors.length))
+// let randonNumberComa = Math.random()*colors.length
+// let randomNumberSinComa = Math.floor(randonNumberComa)
+
+// console.log(randomNumberSinComa)
+
+// bienvenida.style.color = colors[randomNumberSinComa]

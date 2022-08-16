bd529cb41a1aa95d58ebeae18fe00aeddd899d28
Enzovidal10 <enzovidal2001@gmail.com>
2022-08-16 20:58:21 -0300

    Agregué libreria sweet alert

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 
 
-let productos=[
+let iphone=[
     //Iphone XR
     {"img":"https://www.iplace.com.uy/ccstore/v1/images/?source=/file/v574971460940521891/products/100000245.00-iphone-xr-apple-coral-64-gb-mh6r3lz-a.jpg",
      "Modelo":"IPHONE XR",
@@ -9,7 +9,14 @@ let productos=[
      "Camara":"48 MP",
      "Precio":1000},
 
-     {"img":"https://www.iplace.com.uy/ccstore/v1/images/?source=/file/v5883023851070341652/products/100000155.00-iphone-11-aple-negro-64-gb-mhda3lz-a.jpg",
+     {"img":"https://www.iplace.com.uy/ccstore/v1/images/?source=/file/v4205900519121676986/products/100000250.00-iphone-xr-apple-amarillo-128-gb-desbloqueado-mh7p3lz-a.jpg",
+     "Modelo":"IPHONE XR",
+     "Color":"Blanco,Negro",
+     "Almacenamiento":"64GB",
+     "Camara":"64 MP",
+     "Precio":800},
+
+     {"img":"https://www.iplace.com.uy/ccstore/v1/images/?source=/file/v4343811293835771538/products/9999254016239.01-kit-iphone-11-64gb-negro-adaptador-iplace-usb-c.jpg&quality=0.8&outputFormat=JPEG",
      "Modelo":"IPHONE 11",
      "Color":"Azul,Blanco,Negro",
      "Almacenamiento":"128GB",
@@ -21,14 +28,14 @@ let productos=[
      "Modelo":"IPHONE 13",
      "Color":"Dorado,Negro,Azul",
      "Almacenamiento":"64GB",
-     "Camara":"64 MP",
+     "Camara":"12 MP",
      "Precio":1400},
 
      {"img":"https://www.iplace.com.uy/ccstore/v1/images/?source=/file/v8596517346007470358/products/100001673.00-apple-iphone-13-pro-256gb-oro-mlvk3lz-a.jpg",
      "Modelo":"IPHONE 13",
      "Color":"Blanco,Negro",
      "Almacenamiento":"128GB",
-     "Camara":"64 MP",
+     "Camara":"32 MP",
      "Precio":1600},
 
      
@@ -36,23 +43,15 @@ let productos=[
      "Modelo":"IPHONE 12 PRO MAX",
      "Color":"Blanco,Negro,Azul,Dorado",
      "Almacenamiento":"128GB",
-     "Camara":"64 MP",
-     "Precio":1000},
-
-     
-     {"img":"https://www.iplace.com.uy/ccstore/v1/images/?source=/file/v6910719137915295178/products/100000933001.00-usado-iphone-x-256gb-plata.jpg",
-     "Modelo":"IPHONE X",
-     "Color":"Blanco,Negro",
-     "Almacenamiento":"64GB",
-     "Camara":"64 MP",
-     "Precio":800},
+     "Camara":"18 MP",
+     "Precio":1000}
 ];
 
 //Función para crear CARDS automáticas
 
-function cards(productos){
+function cards(iphone){
     let container= document.querySelector('.grid-card');
-    productos.forEach(item => {
+    iphone.forEach(item => {
         container.innerHTML +=
         `
         <div class="card">
@@ -63,12 +62,12 @@ function cards(productos){
                 <ul>
                     <li class="desc">-10% OFF</li>
                     <li class="modelo">${item.Modelo}</li>
-                    <li>Cámara: ${item.Camara}</li>
-                    <li>Color: ${item.Color}</li>
-                    <li>Almacenamiento: ${item.Almacenamiento}</li>
-                    <li class="price">Precio: USD ${item.Precio}</li>
+                    <li><b>Cámara:</b> ${item.Camara}</li>
+                    <li><b>Color:</b> ${item.Color}</li>
+                    <li><b>Almacenamiento:</b> ${item.Almacenamiento}</li>
+                    <li class="price"><b>Precio:</b> USD ${item.Precio}</li>
                 </ul>
-                <input class="btn" type="button" value="Comprar">
+                <input class="btnComprar" type="button" value="Comprar">
             </div>
         </div>
     `
@@ -76,9 +75,10 @@ function cards(productos){
     
 }
 
-cards(productos);
+//Llamar funcion Generar Cards
+cards(iphone);
 
-
+//Barra de Búsqueda (SEARCH)
 document.addEventListener('keyup', e=>{
    
 if (e.target.matches('#buscador')){
@@ -91,5 +91,25 @@ if (e.target.matches('#buscador')){
         :card.classList.add("filtro")
 })
 }
-
 })
+
+
+//Sweet Alert
+const botones=document.querySelectorAll(".btnComprar")
+
+//Recorremos cada boton del array botones(ya que hay muchos con la misma clase)
+for(boton of botones){
+    boton.onclick= (e)=>{
+        //Agregamos la sweet alert para que con el click se ejecute.
+        swal({
+            className: "sweetalert",
+            text:"Has agregado un producto al carrito",
+            icon: "success",
+            button: "OK",
+          });     
+    }
+}
+
+
+
+  
---
+++ js/mac.js
@@ -0,0 +1,108 @@
+//Función de CARDS para Mac
+
+let mac=[
+    //Mackbook
+     {"img":"https://www.iplace.com.uy/ccstore/v1/images/?source=/file/v1982982853025931664/products/100000014.00-imac-apple-21-intelcore-i5-nucleo-mhk03cl-a.jpg",
+     "Modelo":"MACBOOK PRO",
+     "Color":"Blanco,Negro",
+     "DiscoDuro":"1TB",
+     "Procesador":"i5 10th",
+     "Ram":"4GB",
+     "Precio":1800,
+     "Desc":"10%"},
+
+     {"img":"https://www.iplace.com.uy/ccstore/v1/images/?source=/file/v3427391923621716552/products/100001446.00-macbook-pro-retina-apple-13.3-16gb-gris-espacial-1tb-mwp52ci-a-.jpg",
+     "Modelo":"MACBOOK PRO",
+     "Color":"Azul,Dorado,Negro",
+     "DiscoDuro":"500GB",
+     "Procesador":"i5 10th",
+     "Ram":"6GB",
+     "Precio":1000,
+     "Desc":"12%"},
+     
+     {"img":"https://www.iplace.com.uy/ccstore/v1/images/?source=/file/v1877445152997548374/products/100000011.00-imac-apple-24-m1-8nucleos-azul-ssd-512-mgpl3cl-a.jpg",
+     "Modelo":"MACBOOK MINI",
+     "Color":"Azul,Blanco,Negro",
+     "DiscoDuro":"2TB",
+     "Procesador":"i5 10th",
+     "Ram":"12GB",
+     "Precio":1300,
+     "Desc":"20%"},
+     
+     //Iphone 13
+     {"img":"https://www.iplace.com.uy/ccstore/v1/images/?source=/file/v8315679397670854488/products/100001836.00-apple-macbook-pro-14-8cpu-14gpu-16gbram-512gb-gris-espacial-mkgp3ci-a.jpg",
+     "Modelo":"MACBOOK MINI",
+     "Color":"Dorado,Negro,Azul",
+     "DiscoDuro":"640GB",
+     "Procesador":"i5 10th",
+     "Ram":"16GB",
+     "Precio":1400,
+     "Desc":"25%"},
+
+     {"img":"https://www.iplace.com.uy/ccstore/v1/images/?source=/file/v7218542270498287624/products/100001842.00-apple-macbook-pro-14-10cpu-14gpu-16gbram-1tb-plateado-mkgt3ci-a.jpg",
+     "Modelo":"MACBOOK AIR",
+     "Color":"Blanco,Negro",
+     "DiscoDuro":"1TB",
+     "Procesador":"i7 10th",
+     "Ram":"8GB",
+     "Precio":1600,
+     "Desc":"8%"},
+
+     
+     {"img":"https://www.iplace.com.uy/ccstore/v1/images/?source=/file/v6890426951801357837/products/100002112.00-funda-fosca-macbook-14-iplace-ip-1301pf.jpg",
+     "Modelo":"MACBOOK AIR",
+     "Color":"Blanco,Negro,Azul,Dorado",
+     "DiscoDuro":"1TB",
+     "Procesador":"i9 10th",
+     "Ram":"32GB",
+     "Precio":1700,
+     "Desc":"10%"}
+];
+
+//Función para crear CARDS automáticas
+
+function cards(mac){
+    let container= document.querySelector('.grid-card');
+    mac.forEach(item => {
+        container.innerHTML +=
+        `
+        <div class="card">
+            <div class="card-img">
+                <img src="${item.img}" alt="" srcset="">
+            </div>
+            <div class="body-card">
+                <ul>
+                    <li class="desc">${item.Desc}</li>
+                    <li class="modelo">${item.Modelo}</li>
+                    <li><b>Color:</b> ${item.Color}</li>
+                    <li><b>Procesador:</b> ${item.Procesador}</li>
+                    <li><b>Ram:</b> ${item.Ram}</li>
+                    <li><b>Almacenamiento:</b> ${item.DiscoDuro}</li>
+                    <li class="price"><b>Precio:</b> USD ${item.Precio}</li>
+                </ul>
+                <input class="btn" type="button" value="Comprar">
+            </div>
+        </div>
+    `
+    });  
+    
+}
+
+cards(mac);
+
+//Barra de Búsqueda (SEARCH)
+
+document.addEventListener('keyup', e=>{
+   
+    if (e.target.matches('#buscador')){
+    
+        document.querySelectorAll('.card').forEach(card=>{
+    
+            card.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+    
+            ?card.classList.remove("filtro")
+            :card.classList.add("filtro")
+    })
+    }
+    
+    })
